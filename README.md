c3cca671364f89a75220662ae2ee9e0316d60fd0
Sampath Shetty <sshetty@ddd.com>
2017-04-21 07:58:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RiverTechVoice
+Voice recorder app - Base for Opus Codec integration support. 
+- integrate ffmpeg for Opus Codec support. 
---
+++ gen/com/rivertech/VoiceRecorder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.rivertech.VoiceRecorder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
